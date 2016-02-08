57e7f43dbaa6f595c71ef99853d170d7b52535bf
Diogo Monteiro <diogo.arm@gmail.com>
2016-02-07 23:47:11 -0200

    mapbox integration

---
+++ app/src/main/java/diogoarm/android_mapbox_demo/MainActivity.java
@@ -0,0 +1,61 @@
+package diogoarm.android_mapbox_demo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.mapbox.mapboxsdk.constants.Style;
+import com.mapbox.mapboxsdk.geometry.LatLng;
+import com.mapbox.mapboxsdk.views.MapView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private MapView mapView = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mapView = (MapView) findViewById(R.id.mapview);
+        mapView.setStyleUrl(Style.MAPBOX_STREETS);
+        mapView.setLatLng(new LatLng(-15.794, -47.888));
+        mapView.setZoom(11);
+        mapView.onCreate(savedInstanceState);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        mapView.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mapView.onDestroy();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mapView.onPause();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mapView.onResume();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mapView.onStart();
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        mapView.onSaveInstanceState(outState);
+    }
+}
